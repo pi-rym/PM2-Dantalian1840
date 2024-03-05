aa129091d2cae1c62707a84c76670aa20878094a
Dantalian1840 <104804632+Dantalian1840@users.noreply.github.com>
2024-03-04 19:48:06 -0500

    HM1

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,20 @@
-console.log(tempData);
+tempData.map((item) =>{    
+    const movieCard = document.createElement('div');
+    const newTitle = document.createElement('h3');
+    const newPoster = document.createElement('img');
+    const navegator = document.createElement('a');
+
+    movieCard.classList.add('elementos');
+    newTitle.classList.add('card');
+
+    newTitle.innerHTML = item.title;
+    newPoster.src = item.poster;
+    navegator.href = '/front/index.html';
+
+    newTitle.appendChild(navegator);
+    movieCard.appendChild(newTitle);
+    movieCard.appendChild(newPoster);
+
+    const tarjetas = document.getElementById('cards');
+    tarjetas.appendChild(movieCard);
+})
