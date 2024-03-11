54378f81af7cdea74a357ed78b533cc30d284502
Dantalian1840 <104804632+Dantalian1840@users.noreply.github.com>
2024-03-10 23:06:52 -0500

    HM 5

--- challenge-testing/index.js
+++ challenge-testing/index.js
@@ -1,1 +1,35 @@
-const carritoCompra = require("../index");
+class carritoCompra {
+  constructor() {
+    this.products = [];
+    this.id = 0;
+  }
+
+  agregarProducto(name, price, quantity) {
+    this.id++;
+    const product = { name, price, quantity };
+    this.products.push(product);
+  }
+
+  calcularTotal(getProductsFunction) {
+    const items = getProductsFunction();
+    if (items.length === 0) {
+      throw Error("Factura inválida");
+    }
+    let total = 0;
+
+    for (const item of items) {
+      total += item.price * item.quantity;
+    }
+    return total;
+  }
+
+  aplicarDescuento(porcentaje, total) {
+    const dotPercentage = porcentaje / 100;
+    const discount = total * dotPercentage;
+    const totalDescuento = total - discount;
+
+    return totalDescuento;
+  }
+}
+
+module.exports = { carritoCompra };
--- challenge-testing/test/CarritoCompra.test.js
+++ challenge-testing/test/CarritoCompra.test.js
@@ -1,25 +1,49 @@
-class carritoCompra {
-  constructor() {
-    this.activities = [];
-    this.id = 0;
-  }
+const { carritoCompra } = require("../index");
 
-  agregarProducto(product, price, quantity) {
-    this.id++;
-    const carritoCompra = new carritoCompra(product, price, quantity);
-    this.activities.push(producto);
-  }
+const carrito = new carritoCompra();
 
-  calcularTotal() {}
+const mockGetProduct = jest.fn(() => {
+  return [{ name: "ProductoA", price: 10, quantity: 2 }];
+});
 
-  aplicarDescuento(porcentaje) {}
-}
+const mockGetProducts = jest.fn(() => {
+  return [
+    { name: "ProductoA", price: 10, quantity: 2 },
+    { name: "ProductoB", price: 40, quantity: 5 },
+    { name: "ProductoC", price: 15, quantity: 4 },
+  ];
+});
 
-module.exports = carritoCompra;
+const mockEmptyProduct = jest.fn(() => []);
 
-// -- Actividad 4 --
-// Desarrolla una clase en JavaScript llamada CarritoCompra que represente un carrito de compras. La clase debe tener los siguientes métodos:
-// constructor(): Inicializa el carrito como un array vacío.
-// agregarProducto(producto): Recibe un objeto representando un producto y lo agrega al carrito.
-// calcularTotal(): Calcula el total de la compra sumando los precios de todos los productos en el carrito.
-// aplicarDescuento(porcentaje): Aplica un descuento al total de la compra según el porcentaje especificado.
+describe("Función para calcular el total del carrito", () => {
+  it("El callback debería ejecutarse al menos una vez", () => {
+    mockGetProduct();
+    expect(mockGetProduct).toHaveBeenCalled();
+  });
+
+  it("Calcular el total con un único ítem", () => {
+    expect(carrito.calcularTotal(mockGetProduct)).toBe(20);
+  });
+
+  it("Calcular el total con varios ítems", () => {
+    expect(carrito.calcularTotal(mockGetProducts)).toBe(280);
+  });
+
+  it("Ejecutar error si no hay ítems", () => {
+    expect(() => carrito.calcularTotal(mockEmptyProduct)).toThrow(
+      "Factura inválida"
+    );
+  });
+});
+
+describe("Función para calcular el descuento aplicado del carrito", () => {
+  it("Calcular el descuento final del carrito", () => {
+    const total = carrito.calcularTotal(mockGetProducts);
+    expect(carrito.aplicarDescuento(20, total)).toBe(224);
+  });
+
+  it("", () => {});
+});
+
+//Crear el archivo total.js con las funciones para exportar y testear
